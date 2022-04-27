8aa79782dc2be9c0d4c9513be4424b08d207e9bf
Mattcopedia <mathiasofeimun@gmail.com>
2022-04-26 22:50:42 -0700

    First commit

---
+++ src/build/precache-manifest.8f743d972376d479ff883bfd85ac09b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2121fe16cbea9bc077f",
+    "url": "/static/css/main.560cb45e.chunk.css"
+  },
+  {
+    "revision": "a2121fe16cbea9bc077f",
+    "url": "/static/js/main.33f7c5a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "589793a35d4362c5c2bb",
+    "url": "/static/js/2.90d069a7.chunk.js"
+  },
+  {
+    "revision": "b3ca8964400b89166e7c5718b069d7e0",
+    "url": "/index.html"
+  }
+];
